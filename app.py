bb4ef6a0d6cda2be5b4343cc77fe98efbb5d806d
sushiludayakumar <108220128+sushiludayakumar@users.noreply.github.com>
2022-07-10 15:43:53 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import joblib
+import numpy as np
+t_mod=joblib.load("eth_vol (1)")
+st.title("ETHERIUM COIN LOW PRICE PREDICTION!!")
+st.write("""This is basically a website to predict the lowest value that an etherium coin can reach based on ..\nOpen market cost and higest price recorded in a day*""")
+ip1=st.number_input("MARKET OPEN PRICE (in a day)")
+ip2=st.number_input("HIGEST RECORDED PRICE (in a day)")
+ls1=np.append(ip1,ip2)
+op=t_mod.predict([ls1])
+if st.button("PREDICT"):
+  st.title(op)
